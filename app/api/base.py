618fe46c8fb1b2e84c29d75f0f69680bc2b7ea3b
sdaviid <sdaviid@gmail.com>
2022-04-01 22:06:39 -0300

    remove sensor

--- app/api/base.py
+++ app/api/base.py
@@ -1,4 +1,3 @@
-from app.api.routes import route_sensor
 from app.api.routes import route_sprinkler
 from app.api.routes import route_plant
 from app.api.routes import route_waterday
@@ -7,7 +6,6 @@ from fastapi import APIRouter
 
 
 api_router = APIRouter()
-api_router.include_router(route_sensor.router, prefix="/sensor", tags=["sensor"])
 api_router.include_router(route_sprinkler.router, prefix="/sprinkler", tags=["sprinkler"])
 api_router.include_router(route_plant.router, prefix="/plant", tags=["plant"])
 api_router.include_router(route_waterday.router, prefix="/waterday", tags=["waterday"])
